560d825664dedfba4a60bd35b2b1ca09ecfcd192
Sanjeev Kumar <skumar3_ic@fishbowl.com>
2019-08-07 09:50:21 +0530

    fixed bugs related to cookie

--- InAppReporting/src/main/java/com/fb/in/app/reporting/controller/LogoutController.java
+++ InAppReporting/src/main/java/com/fb/in/app/reporting/controller/LogoutController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fb.in.app.reporting.sso.auth.CookieUtil;
+
 /**
  * @author SKumar7
  *
@@ -16,10 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LogoutController {
 	private static final String Logout_Url = "http://10.200.10.21:8081/api/v1/authentication/logout";
+	private static final String jwtTokenCookieName = "JWT-TOKEN";
 
 	@RequestMapping("/logout")
 	public String getReport(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-
+		CookieUtil.clear(httpServletResponse, jwtTokenCookieName);
 		return "redirect:" + Logout_Url;
 	}
 }
--- InAppReporting/src/main/java/com/fb/in/app/reporting/controller/ReportController.java
+++ InAppReporting/src/main/java/com/fb/in/app/reporting/controller/ReportController.java
@@ -3,10 +3,13 @@
  */
 package com.fb.in.app.reporting.controller;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fb.in.app.reporting.sso.auth.JwtUtil;
 
 /**
  * @author SKumar7
@@ -14,8 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ReportController {
+	private static final String jwtTokenCookieName = "JWT-TOKEN";
+	private static final String signingKey = "d652385f5169a19ba3739a0a803396f6e4a5e6f076e3d80f435d6be2324220a8";
+
 	@RequestMapping("/report")
-	public String getReport(HttpServletResponse httpServletResponse) {
-		return "reportDashboard";
+	public String getReport(HttpServletRequest request, Model model) {
+		String username = JwtUtil.getSubject(request, jwtTokenCookieName, signingKey);
+		if (username == null) {
+			model.addAttribute("error", "user is not authorised");
+			return "login";
+		} else
+			return "reportDashboard";
 	}
 }
