5f45787e0bb4588917842482def3739d3ba33fc3
Sanjeev Kumar <43598794+skumar5794@users.noreply.github.com>
2020-03-30 16:25:59 +0530

    path changed

--- src/main/java/com/fb/in/app/reporting/generated/FishbowlSSO.java
+++ src/main/java/com/fb/in/app/reporting/generated/FishbowlSSO.java
@@ -15,7 +15,7 @@ import javax.xml.ws.Service;
  */
 
 //@WebServiceClient(name = "FishbowlSSO", wsdlLocation = "file:/D:/Projects/Fishbowl/FbInsights_Repository/IAR-JAVA/InAppReporting/src/main/resources/wsdl/fishbowlsso.wsdl", targetNamespace = "http://tempuri.org/")
-@WebServiceClient(name = "FishbowlSSO", wsdlLocation = "file:/home/fbpoweruser/IAR-JAVA/InAppReporting/src/main/resources/wsdl/fishbowlsso.wsdl", targetNamespace = "http://tempuri.org/")
+@WebServiceClient(name = "FishbowlSSO", wsdlLocation = "file:/home/fbpoweruser/IAR-JAVA/src/main/resources/wsdl/fishbowlsso.wsdl", targetNamespace = "http://tempuri.org/")
 public class FishbowlSSO extends Service {
 
 	public final static URL WSDL_LOCATION;
@@ -27,7 +27,7 @@ public class FishbowlSSO extends Service {
 		URL url = null;
 		try {
 
-			url = new URL("file:/home/fbpoweruser/IAR-JAVA/InAppReporting/src/main/resources/wsdl/fishbowlsso.wsdl");
+			url = new URL("file:/home/fbpoweruser/IAR-JAVA/src/main/resources/wsdl/fishbowlsso.wsdl");
 			/*
 			 * url = new URL(
 			 * "file:/D:/Projects/Fishbowl/FbInsights_Repository/IAR-JAVA/InAppReporting/src/main/resources/wsdl/fishbowlsso.wsdl"
